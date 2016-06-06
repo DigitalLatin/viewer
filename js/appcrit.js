bcd689bd0fd8274e69db93d4593c99a28df3b944
Hugh Cayless <philomousos@gmail.com>
2016-06-06 13:15:17 -0400

    Modified for front matter.

--- js/appcrit.js
+++ js/appcrit.js
@@ -315,7 +315,7 @@ var copy = function(elt) {
 
 var loadSection = function(id) {
 	var stamp = Date.now();
-	$("tei-div[type=textpart],tei-sourceDesc").css("display", "none");
+	$("tei-div[type=textpart],tei-sourceDesc,tei-front").css("display", "none");
 	if (id) {
 		section = $(id);
 	} else {
@@ -451,10 +451,10 @@ var loadData = function(data) {
 	var nav = $("<div/>", {id:"navigation"});
 	nav.html("<h2>Contents:</h2><ul></ul>");
 	nav.appendTo("body");
+	nav.find("ul").append("<li><a href=\"#front\">Front Matter</a></li>");
 	$("tei-div[type=textpart]").each(function(i, elt) {
 		nav.find("ul").append("<li><a href=\"#" + $(elt).attr("id") + "\">" + $(elt).find("tei-head").html() + "</a></li>");
 	});
-	nav.find("ul").append("<li><a href=\"#sources\">Sources</a></li>");
 	// Add event listeners to ToC
 	$("div#navigation a").click(function(evt) {
 		$("div#navigation a.clicked").removeClass("clicked");
