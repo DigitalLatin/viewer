6be1bdc6ced0fdbe31e8a003fc6e2c9ea778b0bb
Hugh Cayless <philomousos@gmail.com>
2016-08-23 17:30:34 -0400

    Got everything working.

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,11 @@ import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
-  entry: 'src/CETEI.js',
+  entry: 'src/appcrit.js',
   format: 'iife',
-  dest: 'dist/CETEI.js',
+  dest: 'js/appcrit.js',
   sourceMap: false,
-  moduleName: 'CETEI',
+  moduleName: 'appcrit',
   plugins: [
-    babel({exclude: 'node_modules/**', "presets": ["es2015-rollup"]}),
-    uglify()
-  ]
+    babel({exclude: 'node_modules/**', "presets": ["es2015-rollup"]})  ]
 }
