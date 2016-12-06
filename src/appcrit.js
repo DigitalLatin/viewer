5155af82de160c692e6a09c1e5b42c83711ad645
Hugh Cayless <philomousos@gmail.com>
2016-12-06 16:18:27 -0500

    Generalized for texts without textparts.

--- src/appcrit.js
+++ src/appcrit.js
@@ -567,9 +567,14 @@ class appcrit {
 		nav.html("<h2>Contents:</h2><ul></ul>");
 		nav.prependTo("#controls");
 		nav.find("ul").append("<li><a href=\"#front\">Front Matter</a></li>");
-		$(data).find("tei-div[type=textpart]").each(function(i, elt) {
+		let parts = $(data).find("tei-div[type=textpart]");
+		if (parts.length == 0) {
+			parts = $(data).find("tei-div[type=edition]");
+		}
+		parts.each(function(i, elt) {
 			nav.find("ul").append("<li><a href=\"#" + $(elt).attr("id") + "\">" + $(elt).find("tei-head").html() + "</a></li>");
 		});
+
 		nav.find("a").click(function(e) {
 			$("a.selected").toggleClass("selected");
 			$(this).toggleClass("selected");
